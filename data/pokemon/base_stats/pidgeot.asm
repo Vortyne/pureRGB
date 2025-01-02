a3fe21ca9ee72d72e6800a6e5ad34f1395fa0049
SunsetteSkye <53555101+SunsetteSkye@users.noreply.github.com>
2025-01-02 04:37:48 -0600

    Update pidgeot.asm

--- data/pokemon/base_stats/pidgeot.asm
+++ data/pokemon/base_stats/pidgeot.asm
@@ -11,7 +11,7 @@
 	dw PidgeotPicFront, PidgeotPicBackSW
 
 	db TACKLE, NO_MOVE, NO_MOVE, NO_MOVE ; level 1 learnset
-	db GROWTH_MEDIUM_SLOW ; growth rate
+	db GROWTH_SLOW ; growth rate
 
 	tmhm \
 	RAZOR_WIND,\ ; ROOST
